8d5ffa4a1b938d81c5db8052e41155615248e6d1
RyuichiroAbe <66513839+RyuichiroAbe@users.noreply.github.com>
2020-06-30 19:06:45 +0900

    数秒ごとにリクエスト

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function() {
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message_list">
+       `<div class="message_list" data-message-id=${message.id}>
           <div class="message_list__upper">
             <div class="message_list__upper__user_name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function() {
       return html;
     } else {
       var html =
-       `<div class="message_list">
+       `<div class="message_list" data-message-id=${message.id}>
           <div class="message_list__upper">
             <div class="message_list__upper__user_name">
               ${message.user_name}
@@ -65,4 +65,30 @@ $(function() {
       $(".btn").prop("disabled", false);
     });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message_list:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done (function(messages){
+      if (messages.lenght !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i,message){
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail (function(){
+      alert('error');
+    });
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
+  };
 });
